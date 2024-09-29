92a12ed553aaf5edef0375f1ca707a88d3a9ed7d
Sanket Hadap <123326672+snapHH@users.noreply.github.com>
2024-09-30 00:35:59 +0530

    Map Finder

--- src/main/java/com/maps/maps/MapsController.java
+++ src/main/java/com/maps/maps/MapsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MapsController {
  //  Places API Key 
- private static final String API_KEY = "a8b1b164cb664b1b9da4cf8d80d1f1a2";
+ private static final String API_KEY = "Your_API_key";
     
  //  API URL
 private static final String OPENCAGE_API_URL = "https://api.opencagedata.com/geocode/v1/json?q={location}&key={apiKey}";
